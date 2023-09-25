be5d525a38e56d9fa5e379bf6ef6315b87e6a6ab
carolinesouzaa <carolinevmsouza@gmail.com>
2023-09-25 12:14:22 -0300

    Aula do dia 25/09.
    Criação do projeto

---
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -0,0 +1,16 @@
+import styles from './Filtros.module.scss';
+import filtros from './filtros.json'
+
+type IOpcao = typeof filtros[0];
+
+export default function Filtros() {
+    return (
+        <div className={styles.filtro}>
+            {filtros.map((opcao) => (
+                <button className={styles.filtro__filtro} key={opcao.id}>
+                    {opcao.label}
+                </button>
+            ))}
+        </div>
+    )
+}
---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import styles from './Cardapio.module.scss';
+import logo from 'assets/logo.png';
+import Buscador from './Buscador/index';
+import Filtros from './Filtros/index';
+
+export default function Cardapio() {
+  const [busca, setBusca] = useState("");
+    return (
+        <main>
+            <nav className={styles.menu}>
+              <img src= { logo } alt="logo" />
+            </nav>
+            <header className={styles.header}>
+              <div className={styles.header__text}>
+                  Deixando sua vida mais doce!
+              </div>
+            </header>
+            <section className={styles.cardapio}>
+              <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+              <Buscador busca={busca} setBusca={setBusca}/>
+              <div className={styles.cardapio__filtros}>
+                <Filtros/>
+              </div>
+            </section>
+        </main>
+    );
+}
