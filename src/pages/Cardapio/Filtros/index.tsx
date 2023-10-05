cd6aa77e87a621906a484ecd146a12bfe859e3f9
carolinesouzaa <carolinevmsouza@gmail.com>
2023-10-05 11:49:38 -0300

    Aula do dia 05/10.

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -3,11 +3,30 @@ import filtros from './filtros.json'
 
 type IOpcao = typeof filtros[0];
 
-export default function Filtros() {
+interface Props {
+    filtro: number | null;
+    setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+export default function Filtros({filtro, setFiltro}: Props) {
+    function selecionarFiltro(opcao: IOpcao) {
+        return setFiltro(opcao.id)
+    }    
     return (
-        <div className={styles.filtro}>
+        <div className={styles.filtros}>
             {filtros.map((opcao) => (
-                <button className={styles.filtro__filtro} key={opcao.id}>
+                <button
+                className={`
+                ${styles.filtros__filtro}
+                ${
+                    filtro === opcao.id
+                      ? styles["filtros__filtro--ativo"]
+                      : ""
+                  }
+                `}
+                key={opcao.id}
+                onClick={() => selecionarFiltro(opcao)}
+                >
                     {opcao.label}
                 </button>
             ))}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -7,6 +7,7 @@ import Filtros from './Filtros/index';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
     return (
         <main>
             <nav className={styles.menu}>
@@ -15,14 +16,14 @@ export default function Cardapio() {
             </nav>
             <header className={styles.header}>
               <div className={styles.header__text}>
-                  Deixando sua vida mais doce!
+                  Deixando a sua vida mais doce!
               </div>
             </header>
             <section className={styles.cardapio}>
               <h3 className={styles.cardapio__titulo}>Cardápio</h3>
               <Buscador busca={busca} setBusca={setBusca}/>
               <div className={styles.cardapio__filtros}>
-                <Filtros/>
+              <Filtros filtro={filtro} setFiltro={setFiltro}/>
               </div>
             </section>
         </main>
